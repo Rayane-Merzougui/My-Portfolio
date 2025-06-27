0af1448724be6ebcd5ec6ab1be08e87e3e5a16cb
rayane <rayane.merzougui.work@gmail.com>
2025-06-27 16:06:36 +0100

    j'ai crée un logo pour ma page

--- projet-nft/src/pages/Home.js
+++ projet-nft/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navigation from "../components/Navigation";
-
+import Logo from "../components/Logo";
 const Home = () => {
   return (
-    <div className="sec">
-      <Navigation />
-      <h1>Acceil</h1>
+    <div>
+      <Logo />
+      <section className="sec">
+        <Navigation />
+        <h1>Acceil</h1>
+      </section>
     </div>
   );
 };
