58a6798a5348c7e7304b0f906821f8ff8d53a1c2
rayane <rayane.merzougui.work@gmail.com>
2025-07-07 22:15:46 +0100

    j'ai installer openzeppelin et importer sur mon contrat

---
+++ projet-nft/contracts/ProjetNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract ProjetNFT {
+   
+}
