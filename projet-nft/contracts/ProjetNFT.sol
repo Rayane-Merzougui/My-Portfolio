da19d324e8be0325fb99299b2c2ffe3554189f57
rayane <rayane.merzougui.work@gmail.com>
2025-07-08 20:57:53 +0100

    j'avance avec mon contrat

--- projet-nft/contracts/ProjetNFT.sol
+++ projet-nft/contracts/ProjetNFT.sol
@@ -7,6 +7,12 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract ProjetNFT {
-   
+contract ProjetNFT is ERC721URIStorage {
+    address payable owner;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemSold;
+   constructor () ERC721("ProjetNFT", "PNFT") {
+    owner = payable(msg.sender);
+   }
 }
