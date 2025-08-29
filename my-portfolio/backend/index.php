b62ed4fcbb9cb3e253318e24da1b8a1289c16bc2
rayane <rayane.merzougui.work@gmail.com>
2025-08-29 15:19:47 +0100

    j'avance sur mon PHP

--- my-portfolio/backend/index.php
+++ my-portfolio/backend/index.php
@@ -1,16 +1,28 @@
 <?php
-/**$_POST;
-if ($_POST['name']=== ""){
-    echo"Vous avez oublier votre nom d'utilisateur";
-}else if ($_POST['email']=== ""){
-    echo"Vous avez oublier votre email";
-}else if ($_POST['password']=== ""){
-    echo"Mot de passe invalide";
-}else{
-    echo "Inscription validé";
-}**/
 try {
     $databaseConnexion = new PDO(dsn: "mysql:dbname=form-validation", username:'root', password:'');
 }catch(PDOException $exception){
     die("Erreur: {$exception->getMessage()}");
+}
+$emails = $databaseConnexion->query("SELECT email FROM user")->fetchAll(PD0 :: FETCH_ASSOC);
+$allEmails= [];
+$_POST;
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$errors=[];
+if ($name=== ""){
+    echo"Vous avez oublier votre nom d'utilisateur";
+}else if ($email=== ""){
+    echo"Vous avez oublier votre email";
+}else if ($password=== ""){
+    echo"Mot de passe invalide";
+}else{
+    echo "Inscription validé";
+}
+if (mb_strlen($name)<4 || mb_strlen($name)>32 ) {
+    $errors['name'] = "Le nom doit comporter entre 4 et 32 caractères";
+}
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $errors['email']="L'email n'est pas valide";
 }
